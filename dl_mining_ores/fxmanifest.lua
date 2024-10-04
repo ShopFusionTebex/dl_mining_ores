ccfb42fae7ef93b20c91e059796bfa94407d785b
ShopFusionTebex <mariusz.karauda@gmail.com>
2024-10-04 14:39:45 +0200

    Add files via upload

---
+++ dl_mining_ores/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ShopFusion (https://shopfusion.tebex.io/)'
+description 'Mining Rocks by Dont_LookTV'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+
+
+
